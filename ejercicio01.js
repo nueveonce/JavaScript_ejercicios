7df6097d18dc69cd32d663439d502706a4a638d4
Roberto Toledo <toledoroberto@gmail.com>
2022-08-11 23:14:29 -0300

    ejercicios hasta el dos

---
+++ ejercicio01.js
@@ -0,0 +1,4 @@
+var nombre;
+nombre = prompt("¿Como esta el dia hoy?");
+document.write("el dia esta, " + nombre);
+console.log(nombre);
---
+++ ejercicio02.js
@@ -0,0 +1,5 @@
+
+var radio = prompt("Ingrese el radio:");
+document.write("El Area es igual a: " + (Math.PI * Math.pow(radio, 2)));
+document.write("<br>El Perimetro es igual a: " + 2 * Math.PI  * radio);
+ 
