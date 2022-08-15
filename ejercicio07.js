e4b3fbc1d059bd8ce4c8038d62142dd8f2866361
Roberto Toledo <toledoroberto@gmail.com>
2022-08-15 20:37:56 -0300

    ejercicio07

--- ejercicio07.js
+++ ejercicio07.js
@@ -1,14 +1,10 @@
 var valorFinal, valorInicial, acum = 0;
 valorFinal = prompt("Ingrese un valor maximo");
 document.write("El valor final es" + valorFinal + "<BR>");
-console.log(valorInicial);
-console.log(valorFinal);
-console.log(acum);
-valorInicial = prompt("Ingrese un valor:");
-do {
-    
-    console.log(valorInicial);
-    console.log(valorFinal);
-    console.log(acum);
 
-} while (true);
+do {
+    valorInicial = prompt("Ingrese un valor:");    
+    acum=parseInt(acum)+parseInt(valorInicial);    
+    document.write("valor acumulado" + acum + " hasta " + valorFinal + "<br>");
+
+} while (acum<=valorFinal);
