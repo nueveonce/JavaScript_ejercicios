3c23b19c2458bddda89bce2b1dc7b1130bebb55d
Roberto Toledo <toledoroberto@gmail.com>
2022-08-16 21:47:29 -0300

    ejercicio 08

--- ejercicio08.js
+++ ejercicio08.js
@@ -1,14 +1,23 @@
-var minimo, maximo, promedio, numero, contador = 0, suma=0;
+var minimo;
+var maximo;
+var promedio = 0;
+var numero, contador = 1, suma = 0;
+numero = ingresarValor(numero);
+minimo = numero;
+maximo = numero;
+suma = numero;
 do {
     contador++;
-    numero = prompt("ingrese un numero entero ( " + contador + " ): ");
-    numero = parseInt(numero);
-    suma=parseInt(suma)+numero;
-    asignacionValores(numero, contador);
-    maximo=valorMaximo(numero);
-    minimo=valorMinimo(numero);
-    promedio=suma/parseInt(contador);
-    
+    numero = ingresarValor(numero);
+    console.log("suma = " + suma + "numero = " + numero);
+    suma = parseInt(suma) + numero;
+    console.log("suma total = " + suma);
+    console.log("contador = " + contador);
+
+    maximo = valorMaximo(numero);
+    minimo = valorMinimo(numero);
+    promedio = suma / parseInt(contador-1);
+
 } while (numero !== 0);
 
 document.write("El valor maximo es: " + maximo);
@@ -16,25 +25,22 @@ document.write("<br>El valor minimo es: " + minimo);
 document.write("<br>El valor Promedio es: " + promedio);
 
 function valorMaximo(nunero) {
-  
-    if (numero>maximo) {
+    if (numero > maximo && numero != 0) {
         return parseInt(numero);
     } else {
         return parseInt(maximo);
     }
 }
 function valorMinimo(numero) {
-  
-    if (numero<minimo) {
+
+    if (numero < minimo && numero != 0) {
         return parseInt(numero);
     } else {
         return parseInt(minimo);
     }
 }
-function asignacionValores(numero, contador) {
-    if (contador==1) {
-        maximo=numero;
-        minimo=numero;        
-    }
-    
+function ingresarValor(numero) {
+    numero = prompt("ingrese un numero entero");
+    numero = parseInt(numero);
+    return numero;
 }
