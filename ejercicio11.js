bcba7480d542b83e69f9c1965f42d13db38f9c7b
Roberto Toledo <toledoroberto@gmail.com>
2022-08-19 23:25:29 -0300

    ejercicio 11

---
+++ ejercicio11.js
@@ -0,0 +1,16 @@
+var cadena = prompt("INGRESE UNA FRASE");
+var palabraMasLarga = 0, cantidadCaracteres, posicion;
+cadena2 = cadena.split(" ");
+console.log(cadena2);
+buscarPalabraMasLarga();
+function buscarPalabraMasLarga() {
+    for (let index = 0; index < cadena2.length; index++) {
+        cantidadCaracteres = cadena2[index].length;
+        if (palabraMasLarga < cantidadCaracteres) {
+            palabraMasLarga = cantidadCaracteres
+            posicion = index;
+        }
+    }
+}
+
+document.write(cadena2[posicion]);
