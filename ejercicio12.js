a3e94ddf589b4e2dc2506e06b2985f432027f929
Roberto Toledo <toledoroberto@gmail.com>
2022-08-21 23:08:10 -0300

    ejercicio hasta 13

---
+++ ejercicio12.js
@@ -0,0 +1,12 @@
+var dato = 12347;
+document.write(typeof dato);
+
+
+
+var x = 12345; 
+document.write(typeof x) // number
+x = 'string'; 
+console.log(typeof x) // string
+x = { key: 'value' };
+console.log(typeof x) // object
+
